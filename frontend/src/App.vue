11ecf40a7b686b5c4da5521adf48d1490b73809f
Me <fambopes@protonmail.com>
2022-07-11 20:46:12 +0200

    Added vue js support

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <testcomponent />
+  </div>
+</template>
+
+<script>
+import testcomponent from './components/testcomponent'
+
+export default {
+  name: 'App',
+  components: {
+    testcomponent
+  }
+}
+
+// sessionStorage.setItem("key", "value") // TODO this will be the way of storing game data
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/testcomponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1 @click="onClick()">{{ counter }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    onClick () {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
